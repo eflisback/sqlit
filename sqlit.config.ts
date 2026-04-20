1384476af53174424324109677228b62100fcc28
Ebbe Flisbäck <ebbe.flisback@gmail.com>
2026-04-20 23:59:33 +0200

    Example sqlit config file

--- sqlit.config.ts
+++ sqlit.config.ts
@@ -1,7 +1,16 @@
 import type { SqlitConfig } from '@/lib/types/config';
 
 const config: SqlitConfig = {
-	sheets: [],
+	sheets: [
+		{
+			name: 'Welcome to sqlit',
+			description: '',
+			source: {
+				type: 'url',
+				url: 'https://raw.githubusercontent.com/eflisback/sqlit/8070c3dfc1f5b04cbc1713e9d8d025fe1ee867cd/src/store/welcome.sqlit.md'
+			}
+		}
+	],
 };
 
 export default config;
